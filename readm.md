11cd75b21626338584a4e58c0622952cee47f334
AttaMishwani <atta.rehmanmishwani@gmail.com>
2024-09-25 15:51:21 +0500

    .

--- readm.md
+++ readm.md
@@ -1,1 +1,24 @@
 https://umwteam.net/24/kati/
+
+ <div class="swiper mySwiper">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide" width="400px">
+              <img src="images/event-1.jpg" alt="" />
+            </div>
+            <div class="swiper-slide" width="400px">
+              <img src="images/event-2.jpg" alt="" />
+            </div>
+            <div class="swiper-slide" width="400px">
+              <img src="images/event-3.jpg" alt="" />
+            </div>
+            <div class="swiper-slide" width="400px">
+              <img src="images/event-1.jpg" alt="" />
+            </div>
+            <div class="swiper-slide" width="400px">
+              <img src="images/event-2.jpg" alt="" />
+            </div>
+          </div>
+          <div class="swiper-button-next"></div>
+          <div class="swiper-button-prev"></div>
+          <!-- <div class="swiper-pagination"></div> -->
+        </div>
---
+++ script.js
@@ -0,0 +1,82 @@
+// script.js
+let toggleBtn = document.querySelector(".toggle-btn");
+let lowerMenu = document.getElementById("lower-menu");
+let toggleButtonIcon = document.querySelector(".toggle-btn .button");
+
+toggleBtn.addEventListener("click", () => {
+  console.log("working");
+  if (lowerMenu.className !== "active") {
+    lowerMenu.classList.add("active");
+    toggleButtonIcon.src = "images/xmarktwo.png";
+    console.log("class added");
+  } else {
+    lowerMenu.classList.remove("active");
+    toggleButtonIcon.src = "images/menuOne.png";
+    console.log("class removed");
+  }
+});
+
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  direction: getDirection(),
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+    },
+  },
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? "vertical" : "horizontal";
+  return direction;
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+
+  // Trigger GSAP animation for the current slide's text
+  gsap.from(slides[slideIndex - 1].querySelector(".slide-text"), {
+    y: 150,
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out",
+  });
+}
